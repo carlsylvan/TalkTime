180b407e1f99ba3e7b3b7d9373dbf5753960e490
sergejsvoronins <svoronins@gmail.com>
2023-08-25 11:23:44 +0200

    feature to join rooms is added

--- client/src/components/ActiveRooms/ActiveRooms.tsx
+++ client/src/components/ActiveRooms/ActiveRooms.tsx
@@ -4,14 +4,14 @@
 import { CreateRoom } from "../CreateRoom";
 import "./ActiveRooms.scss";
 import { IChatGroup } from "../../models/IChatGroup";
+import socket from "../../socket/socket";
 
 interface IActiveRoomsProps {
     chatGroups: IChatGroup []
 }
 export const ActiveRooms = (props:IActiveRoomsProps) => {
-  const handleClick = (group: string) => {
-
-    console.log("Joined: ", group);
+  const handleClick = (groupId: string) => {
+    socket.emit("join_group", groupId);
   };
   return (
     <div className="rooms">
@@ -23,7 +23,7 @@ export const ActiveRooms = (props:IActiveRoomsProps) => {
             <li
               key={i}
               onClick={() => {
-                handleClick(e.name);
+                handleClick(e.id);
               }}
             >
               {e.name} ({e.users.length} personer)
